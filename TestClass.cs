2f795ae97ae4eba67ed8a3c131c9bf78c6e1e984
ioana-b <53857844+ioana-b@users.noreply.github.com>
2021-04-09 16:14:55 +0300

    Add files via upload

---
+++ TestClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp
+{
+    public class TestClass
+    {
+        private string Parameter;
+
+        public TestClass(string value)
+        {
+            Parameter = value;
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine($"I'm doing something with this value: {Parameter}");   
+        }
+    }
+}
